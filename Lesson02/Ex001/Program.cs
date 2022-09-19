60c58ba9a10b71587314716d6291c8465db9b9bf
Your Name~ <you@example.com>
2022-09-19 18:29:02 +0300

    L2Ex001

--- Lesson02/Ex001/Program.cs
+++ Lesson02/Ex001/Program.cs
@@ -1,24 +1,14 @@
-﻿int a1 = 15;
-int b1 = 21;
-int c1 = 39;
-int a2 = 12;
-int b2 = 23;
-int c2 = 33;
-int a3 = 13;
-int b3 = 23;
-int c3 = 33;
-
-int max = a1;
-if(b1 > max) max = b1;
-if (c1 > max) max = c1;
-
-if(a2 > max) max a1;
-if (b2 > max) max b1;
-if (c2 > max) max c1;
+﻿int Max(int  arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {11,21,31,41,5,6,17,18,19};
+array[0] = 12;
+Console.WriteLine(array[4]);
 
 
-if(a3 > max) max a3;
-if (b3 > max) max b3;
-if (c3 > max) max c3;
 
-Console.WriteLine(max);
+   
